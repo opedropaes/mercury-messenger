b95c4f9c3c0077d92ea93b02298ac2247985431f
Hi-im-Yan <yanajiki2010@hotmail.com>
2019-10-21 14:30:09 -0400

    Added authentication scripts

--- front/auth/server.js
+++ front/auth/server.js
@@ -1,16 +1,48 @@
+//Para rodar sapoha tem que escrever o seguinte no terminal:
+//npm run-script devStart       ou        npm devStart
+//Isso vai rodar o nodemon e o server.js
+
+//Instalem a extensão Rest Cliente no VSCode
+//No arquivo request.rest existe GET e POST, logo em cima existe a opção "send request"
+//Essa opção é um botão, cliquem nela para fazer uma request no servidor.
+
 const express = require("express");
-const path = require("path");
-const http = require("http");
-const socket = require("socket.io");
-
 const app = express();
-const server = http.createServer(app);
-const io = socket(server);
+const bcrypt = require("bcrypt");
 
-const users = [{ name: "yan" }];
+app.use(express.json());
+
+const users = [];
 
 app.get("/users", (req, res) => {
-  res.text(users);
+  res.json(users);
+});
+
+app.post("/users", async (req, res) => {
+  try {
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+    const user = { name: req.body.name, password: hashedPassword };
+    users.push(user);
+    res.status(201).send();
+  } catch {
+    res.status(500).send();
+  }
+});
+
+app.post("/users/login", async (req, res) => {
+  const user = users.find(user => user.name === req.body.name);
+  if (user == null) {
+    return res.status(400).send("Cannot find user");
+  }
+  try {
+    if (await bcrypt.compare(req.body.password, user.password)) {
+      res.send("Success");
+    } else {
+      res.send("Not Allowed");
+    }
+  } catch {
+    res.status(500).send();
+  }
 });
 
 app.listen(8080);
