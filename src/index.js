98c8dda4a32329e8f27a5ee0d22a490fa9244d95
Pedro Augusto <pedroaugusto@jera.com.br>
2019-10-26 16:41:41 -0400

    alinhando com o front

--- src/index.js
+++ src/index.js
@@ -1,53 +1,76 @@
-const app = require('./config/server').app;
-const server = require('./config/server').server;
+const app = require("./config/server").app;
+const server = require("./config/server").server;
 const io = require("./config/server").io;
 const port = process.env.PORT || 3000;
-const expressStatusMonitor = require('express-status-monitor');
-const bodyParser = require('body-parser');
-const router = require('./routes/router');
+const expressStatusMonitor = require("express-status-monitor");
+const bodyParser = require("body-parser");
+const router = require("./routes/router");
 
 server.listen(port, () => {
-	console.log(`Server started on port ${port}`);
+  console.log(`Server started on port ${port}`);
 });
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(expressStatusMonitor({ websocket: io, port: app.get('port') }));
+app.use(expressStatusMonitor({ websocket: io, port: app.get("port") }));
 app.use(router);
 
 // Array de mensagens passadas provisório => será substituido pelas mensagens no drive
 let previousMessagesArray = [];
-
 let rooms = {};
 
+let connectedGlobalRoomSockets = [];
+
+const broadCastGlobal = message => {
+  connectedGlobalRoomSockets
+    .map(el => el.socket)
+    .forEach(socket => writeGlobalMessage(socket, message));
+};
+
+const writeGlobalMessage = (socket, message) => {
+  console.log("emiting global");
+  socket.emit("globalMessage", JSON.stringify(message));
+};
+
 // Eventos do socket quando este é iniciado
-io.on('connection', socketClient => {
+io.on("connection", socketClient => {
+  socketClient.on("userConnectedToRoom", roomConnectionData => {
+    let { sender, receiver, room } = roomConnectionData;
 
-	socketClient.on('userConnectedToRoom', roomConnectionData => {
+    // Sender conectado na sala
+    socketClient.join(room);
 
-		let { sender, receiver, room } = roomConnectionData;
+    console.log(`${sender} joined ${room} as sender to ${receiver}`);
 
-		// Sender conectado na sala
-		socketClient.join(room);
+    rooms[room] = { users: [] };
+    rooms[room].users[socketClient.id] = sender;
+  });
 
-		console.log(`${sender} joined ${room} as sender to ${receiver}`);
+  socketClient.on("connectToGlobal", params => {
+    let { sender } = params;
+    let socket = { user: sender, socket: socketClient };
+    if (!connectedGlobalRoomSockets.includes(socket)) {
+      console.log(`${sender} joined global room`, params);
+      connectedGlobalRoomSockets.push(socket);
+    }
+  });
 
-		rooms[room] = { users: [] };
-		rooms[room].users[socketClient.id] = sender;
+  socketClient.on("sendMessageGlobal", data => {
+    console.log("client trying to send global message", data);
+    let { message } = data;
+    broadCastGlobal(message);
+  });
 
-	});
+  socketClient.on("sendMessage", data => {
+    let { author, message, room } = data;
+    previousMessagesArray.push({ author, message, room }); // vai sumir depois pra virar persistencia no drive
+    socketClient.to(room).broadcast.emit("receivedMessage", data);
+  });
 
-	socketClient.on('sendMessage', data => {
-		let { author, message, room } = data;
-		previousMessagesArray.push({ author, message, room }); // vai sumir depois pra virar persistencia no drive
-		socketClient.to(room).broadcast.emit('receivedMessage', data);
-	});
+  socketClient.emit("previousMessages", previousMessagesArray);
 
-	socketClient.emit('previousMessages', previousMessagesArray);
-
-	socketClient.on('disconnect', () => {
-		console.info(`Client disconnected: ${socketClient.id}`);
-		// TODO: chamar lastSeenController e mudar pro instante corrente
-	});
-
+  socketClient.on("disconnect", () => {
+    console.info(`Client disconnected: ${socketClient.id}`);
+    // TODO: chamar lastSeenController e mudar pro instante corrente
+  });
 });
