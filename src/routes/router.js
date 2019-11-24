57d6ec202fdfbbc345272f05be4159ff46ed328d
Pedro <pee.dro_fs@hotmail.com>
2019-11-24 18:49:58 -0400

    frontend update

--- src/routes/router.js
+++ src/routes/router.js
@@ -5,52 +5,53 @@ const contactController = require('../controllers/contactsController').contactSe
 const communicatorController = require('../controllers/communicatorController').communicatorServices;
 
 router.get('/', (req, res) => {
-	res.send('mercury-messenger');
+    res.send('mercury-messenger');
 });
 
-router.post('/registrar', async (req, res) => {
-	let isRegistered = false
-	authController.register(req, res)
-		.then(response => {
-			const { communicator, token } = response;
-			isRegistered = communicator.id;
+router.post('/registrar', async(req, res) => {
+    let isRegistered = false
+    authController.register(req, res)
+        .then(response => {
+            console.log(response)
+            const { communicator, token } = response;
+            isRegistered = communicator.id;
 
-			if (isRegistered) {
-				res.status(200).json({ communicator, token });
-			} else {
-				res.status(400).send(communicator);
-			}
+            if (isRegistered) {
+                res.status(200).json({ communicator, token });
+            } else {
+                res.status(400).send(communicator);
+            }
 
-		})
-		.catch(err => {
-			console.log(err);
-		});
+        })
+        .catch(err => {
+            console.log(err);
+        });
 });
 
-router.post('/entrar', async (req, res) => {
-	let isLoginValid = false 
-	authController.login(req, res)
-		.then(response => {
-			try {
-				isLoginValid = response.communicator;
-				if (isLoginValid) {
+router.post('/entrar', async(req, res) => {
+    let isLoginValid = false
+    authController.login(req, res)
+        .then(response => {
+            try {
+                isLoginValid = response.communicator;
+                if (isLoginValid) {
 
-					const { communicator, token } = response;
-					const { username } = communicator;
-					res.status(200).json({ username, token });
-				
-				} else {
-					res.status(400).send(response);
-				}
-			} catch (error) {
-				console.log("error from catch: ", error);
-				res.status(404).send(response);
-			}
-			
-		})
-		.catch(err => {
-			console.log(err);
-		});
+                    const { communicator, token } = response;
+                    const { username } = communicator;
+                    res.status(200).json({ username, token });
+
+                } else {
+                    res.status(400).send(response);
+                }
+            } catch (error) {
+                console.log("error from catch: ", error);
+                res.status(404).send(response);
+            }
+
+        })
+        .catch(err => {
+            console.log(err);
+        });
 });
 
 router.delete('/excluir', authController.delete);
