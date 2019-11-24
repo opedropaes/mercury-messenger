7663a155ebdada68254889fda4d0e2acd5cd835d
Pedro Mihael <pedromvpaes@gmail.com>
2019-11-24 16:07:21 -0400

    Add update and delete account routes

--- src/routes/router.js
+++ src/routes/router.js
@@ -53,19 +53,53 @@ router.post('/entrar', async (req, res) => {
 		});
 });
 
-router.delete('/excluir', authController.delete);
-
-router.get('/pessoas', authServices.list);
 
 // Contatos
 
-router.post('/adicionar-contato', contactServices.addContact);
+router.post('/adicionar-contato', contactController.addContact);
 
-router.delete('/remover-contato', contactServices.removeContact);
+router.delete('/remover-contato', contactController.removeContact);
 
 // User
 
-router.get('/:username/listar-contatos', communicatorServices.getContacts);
+router.get('/pessoas', communicatorController.listAllCommunicators);
+
+router.get('/:username/listar-contatos', communicatorController.getContacts);
+
+router.post('/:username/configurar-conta', async (req, res) => {
+	const { username } = req.params;
+	const { newName, newEmail } = req.body;
+	const payload = { username, newName, newEmail };
+
+	communicatorController.updateAccout(payload)
+		.then(response => {
+			if (response.ok == 1) {
+				res.status(200).send(response);
+			} else {
+				res.status(400).send(response);
+			}
+		})
+		.catch(err => {
+			console.log(err)
+			res.status(500).send(err);
+		});
+});
+
+router.delete('/:username/deletar-conta', async (req, res) => {
+	const { username } = req.params;
+	communicatorController.deleteAccount(username)
+		.then(response => {
+			if (response.ok == 1) {
+				res.status(200).send(response);
+			} else {
+				res.status(400).send(response);
+			}
+		})
+		.catch(err => {
+			console.log(err)
+			res.status(500).send(err);
+		});
+});
 
 
 module.exports = router;
